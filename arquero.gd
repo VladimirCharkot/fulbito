c60253cae5ca1bb9aa660843bcb9735e6e8a64d2
VladimirCharkot <vlad.chk@gmail.com>
2024-06-26 19:17:14 -0300

    Arquero

--- arquero.gd
+++ arquero.gd
@@ -1,20 +1,32 @@
 extends Jugador
 
 var area: Area2D = null
+var arco: Area2D = null
+var pelotapos: Node2D = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
+var v = Vector2.ZERO
+var punto_deseado = Vector2.ZERO
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
-	var v = Vector2(1, 0)
-	position += v * vel
-	if area and not(area.overlaps_area($Control)):
-		position -= v * vel
+	
+	var arco_pelota = arco.position.direction_to(pelotapos.position)
+	punto_deseado = arco.position + arco_pelota * 100
+	v = position.direction_to(punto_deseado)
+	
+	if position.distance_to(punto_deseado) > 2:
+		velocity = v * vel
+	else:
+		velocity = Vector2.ZERO
 		
+	move_and_slide()
+
 	apariencia(v)
 	
 	manejar_pelota(v)
+	
+	queue_redraw()
+
+func _draw():
+	draw_circle(punto_deseado, 5, Color.BROWN)
+	draw_line(Vector2.ZERO, velocity, Color.DARK_BLUE, 3)
--- cancha.gd
+++ cancha.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Arquero.area = $AreaIzq
+	$Arquero.pelotapos = $Pelota
+	$Arquero.arco = $ArcoIzq
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
